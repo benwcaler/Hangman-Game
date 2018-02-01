06cb6790ddb208c2458443e7b634d5c185c3dda2
Ben <b.wayne@me.com>
2018-01-31 18:33:42 -0700

    started html and js code

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+//list of words to pull from randomly
+var dinosaurs = ["utahraptor", "triceratops", "velociraptor", "tyranosaurus rex", "stegosaurus", "giraffatitan", "zephyrosaurus", "quaesitosaurus", "brachiosaurus", "megalosaurus"]
+
+//press any key to select a random word
+
+//display a series of dashes depending on the length of the random word array
+
+//when a key is pressed, check to see if the key is in the random word array
+
+//if the letter is in the word, replace the dash with the letter for each time the letter is in the word
+
+//if the letter is not in the word send it to a dead letter array and display it in the letter graveyard
+
+//once twelve dead letters are selected the game is over
+
+//select a new word once the previous word is guessed or the game ends due to dead letters
