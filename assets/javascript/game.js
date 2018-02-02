a01c75b5b0e54bc5cbb20c42fa208141d4dcd0ce
Ben <b.wayne@me.com>
2018-02-02 13:08:59 -0700

    more progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,11 +3,14 @@ var dinosaurs = ["utahraptor", "triceratops", "velociraptor", "tyranosaurus rex"
 var gameWord = []
 var blanks = []
 var letterGrave = []
+var wins = 0
+var loses = 0
 
 $(document).ready(function () {
-    document.onkeyup = function hangManGame() {
+    document.onkeypress = function hangManGame() {
 
         //press any key to select a random word
+
         gameWord = document.getElementById("word").innerHTML = dinosaurs[Math.floor(Math.random() * 10)];
         //display a series of blanks depending on the length of the random word array
         blanks = [];
@@ -17,31 +20,50 @@ $(document).ready(function () {
                 blanks[i] = (" ");
             }
         }
+        
         document.getElementById("blanks").innerHTML = blanks.join(" ");
         //when a key is pressed, check to see if the key is in the random word array
         //if the letter is in the word, replace the dash with the letter for each time the letter is in the word
         //if the letter is not in the word send it to a dead letter array and display it in the letter graveyard
-        document.onkeyup = function (guess) {
+        document.onkeypress = function (guess) {
             attempt = guess.key;
-            for (var i = 0; i < blanks.length; i++) {
-                if (attempt === gameWord[i]) {
-                    blanks[i] = attempt;
-                    console.log("letter is there")
-                    document.getElementById("blanks").innerHTML = blanks.join(" ");
+            if (letterGrave.includes(attempt) || blanks.includes(attempt)) {
+                alert("You've already guess that. ")
+            }
+            if (gameWord.includes(attempt)) {
+                for (var i = 0; i < blanks.length; i++) {
+                    if (attempt === gameWord[i]) {
+                        blanks[i] = attempt;
+                        console.log("letter is there")
+                        document.getElementById("blanks").innerHTML = blanks.join(" ");
+                    }
+                }
+            } else {
+                if (letterGrave.includes(attempt)) {
                 } else {
                     letterGrave.push(attempt)
                     console.log("letter is not there")
                     document.getElementById("letterGrave").innerHTML = letterGrave.join(" ")
                 }
             }
+            console.log(letterGrave)
+            //once twelve dead letters are selected the game is over
+            if (letterGrave.length === 12) {
+                loses ++
+                alert("Game Over" + "You have " + loses + " loses!");
+                console.log(blanks)
+                console.log(gameWord)
+            } else if (blanks.join("") === gameWord) {
+                wins++
+                alert("You win!" +"You have " + wins + " wins!");
+            }
+
         }
-
-
-        //once twelve dead letters are selected the game is over
-
         //select a new word once the previous word is guessed or the game ends due to dead letters
         console.log(gameWord)
         console.log(blanks)
+
     }
+    // document.onkeyup(hangManGame())
 });
 
