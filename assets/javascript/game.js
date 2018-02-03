69c2174fb0a1c649ead81d4d96e751bbfd6a676b
Ben <b.wayne@me.com>
2018-02-02 18:19:45 -0700

    finished game. Still need css and html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,65 +5,72 @@ var blanks = []
 var letterGrave = []
 var wins = 0
 var loses = 0
+var guesses = 0
 
 $(document).ready(function () {
-    document.onkeypress = function hangManGame() {
-
-        //press any key to select a random word
-
-        gameWord = document.getElementById("word").innerHTML = dinosaurs[Math.floor(Math.random() * 10)];
-        //display a series of blanks depending on the length of the random word array
+    //starts the game with a random key
+    document.onkeypress = function start() {
+        reset()
+        hangManGame()
+    }
+    //resets all variables and chooses the word
+    function reset() {
+        gameWord = [];
         blanks = [];
+        letterGrave = [];
+        guesses = 12;
+        document.getElementById("guesses").innerHTML = guesses
+        gameWord = document.getElementById("word").innerHTML = dinosaurs[Math.floor(Math.random() * 10)];
+        document.getElementById("letterGrave").innerHTML = letterGrave.join(" ")
+        //display a series of blanks depending on the length of the random word array
         for (var i = 0; i < gameWord.length; i++) {
             blanks.push("_");
             if (gameWord[i] === " ") {
-                blanks[i] = (" ");
+                blanks[i] = ("-");
             }
         }
-        
         document.getElementById("blanks").innerHTML = blanks.join(" ");
-        //when a key is pressed, check to see if the key is in the random word array
-        //if the letter is in the word, replace the dash with the letter for each time the letter is in the word
-        //if the letter is not in the word send it to a dead letter array and display it in the letter graveyard
+    }
+
+    function hangManGame() {
         document.onkeypress = function (guess) {
             attempt = guess.key;
-            if (letterGrave.includes(attempt) || blanks.includes(attempt)) {
+            if (letterGrave.includes(attempt.toUpperCase()) || blanks.includes(attempt)) {
                 alert("You've already guess that. ")
             }
+            //when a key is pressed, check to see if the key is in the random word array
+            //if the letter is in the word, replace the dash with the letter for each time the letter is in the word
             if (gameWord.includes(attempt)) {
                 for (var i = 0; i < blanks.length; i++) {
                     if (attempt === gameWord[i]) {
                         blanks[i] = attempt;
-                        console.log("letter is there")
                         document.getElementById("blanks").innerHTML = blanks.join(" ");
                     }
                 }
             } else {
-                if (letterGrave.includes(attempt)) {
+                //if the letter is not in the word send it to a dead letter array and display it in the letter graveyard
+                if (letterGrave.includes(attempt.toUpperCase())) {
                 } else {
-                    letterGrave.push(attempt)
-                    console.log("letter is not there")
+                    guesses --
+                    document.getElementById("guesses").innerHTML = guesses
+                    letterGrave.push(attempt.toUpperCase())
                     document.getElementById("letterGrave").innerHTML = letterGrave.join(" ")
                 }
             }
-            console.log(letterGrave)
             //once twelve dead letters are selected the game is over
             if (letterGrave.length === 12) {
-                loses ++
-                alert("Game Over" + "You have " + loses + " loses!");
-                console.log(blanks)
-                console.log(gameWord)
+                loses++
+                alert("Game Over " + "You have " + loses + " loses!");
+                //select a new word once the previous word is guessed or the game ends due to dead letters
+                reset()
+                //once the word is finished the game is over
             } else if (blanks.join("") === gameWord) {
                 wins++
-                alert("You win!" +"You have " + wins + " wins!");
+                alert("You win! " + "You have " + wins + " wins!");
+                //select a new word once the previous word is guessed or the game ends due to dead letters
+                reset()
             }
-
         }
-        //select a new word once the previous word is guessed or the game ends due to dead letters
-        console.log(gameWord)
-        console.log(blanks)
-
     }
-    // document.onkeyup(hangManGame())
 });
 
